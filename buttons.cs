290bee9521c78dbf3fc42d4cc2759fce6ee3fec1
AdvellerdC <84400051+AdvellerdC@users.noreply.github.com>
2021-09-16 21:59:13 +0400

    .

---
+++ buttons.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.Net;
+using Discord.WebSocket;
+using Discord.Net.WebSockets;
+using Discord;
+using System.Threading;
+namespace buttons
+{
+    public class buttons : ModuleBase<SocketCommandContext>
+    {
+        [Command("button")]
+        public async Task Button() {
+            var builder = new ComponentBuilder().WithButton("Ewa", customId: "id_1", ButtonStyle.Primary, row: 0);
+            await Context.Channel.SendMessageAsync("UwU", component: builder.Build());
+
+        }
+    }
+}
