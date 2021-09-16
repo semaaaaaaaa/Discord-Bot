a3a78c884b765c91af8beeca71ba7a820bcc975d
AdvellerdC <84400051+AdvellerdC@users.noreply.github.com>
2021-09-16 22:00:48 +0400

    .

---
+++ giveremoverole.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.Net;
+using Discord.WebSocket;
+using Discord.Net.WebSockets;
+using Discord;
+namespace giveremove
+{
+    public class giveremove : ModuleBase<SocketCommandContext>
+    {
+        [Command("giverole")]
+        [RequireUserPermission(GuildPermission.BanMembers, ErrorMessage = "You don't have rights for this role!")]
+        public async Task GiveRole(IGuildUser user, [Remainder] string roles)
+        {
+
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == roles);
+            await (user as IGuildUser).AddRoleAsync(role);
+            await ReplyAsync($"Done! {role.ToString()} for {user.Mention} was added!");
+        }
+        [Command("removerole")]
+        [RequireUserPermission(GuildPermission.BanMembers, ErrorMessage = "You don't have rights for this role!")]
+        public async Task RemoveRole(IGuildUser user=null, [Remainder] string roles=null)
+        {
+
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == roles);
+            await (user as IGuildUser).RemoveRoleAsync(role);
+            await ReplyAsync($"Done! {role.ToString()} from {user.Mention} was deleted");
+        }
+    }
+}
