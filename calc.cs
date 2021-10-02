a4fb70de47ff85c5dbbb238ffe5757f7478840f5
AdvellerdC <84400051+AdvellerdC@users.noreply.github.com>
2021-10-02 18:27:50 +0400

    .

--- calc.cs
+++ calc.cs
@@ -15,7 +15,7 @@ namespace calc
     {
         [Command("calc")]
         public async Task Calc() {
-            var builder = new ComponentBuilder().WithButton("Hello!", customId: "id_1", ButtonStyle.Secondary, row: 0);
+            var builder = new ComponentBuilder().WithButton("Hello!", customId: "id_1", ButtonStyle.Secondary, row: 0).WithButton("Hello!", customId: "id_1", ButtonStyle.Secondary, row: 0);
             await Context.Channel.SendMessageAsync("Test buttons!", component: builder.Build());
 
         }
