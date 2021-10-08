9b0faa8f17999364dcc555e49e60d7121423627e
AdvellerdC <84400051+AdvellerdC@users.noreply.github.com>
2021-10-08 18:42:06 +0400

    .

--- calc.cs
+++ calc.cs
@@ -14,12 +14,63 @@ namespace calc
     public class calc : ModuleBase<SocketCommandContext>
     {
         [Command("calc")]
-        public async Task Calc() {
-            for (int i = 0; i < 3; i++)
+        [Alias("resources")]
+        public async Task Resources()
+        {
+
+            ComponentBuilder builder = new ComponentBuilder();
+
+            builder.WithButton("1", "1", ButtonStyle.Primary, null, null, false, row: 0);
+            builder.WithButton("2", "2", ButtonStyle.Primary, null, null, false, row: 0);
+            builder.WithButton("3", "3", ButtonStyle.Primary, null, null, false, row: 0);
+            builder.WithButton("4", "4", ButtonStyle.Primary, null, null, false, row: 0);
+            builder.WithButton("5", "5", ButtonStyle.Primary, null, null, false, row: 0);
+            builder.WithButton("6", "6", ButtonStyle.Primary, null, null, false, row: 1);
+            builder.WithButton("7", "7", ButtonStyle.Primary, null, null, false, row: 1);
+            builder.WithButton("8", "8", ButtonStyle.Primary, null, null, false, row: 1);
+            builder.WithButton("9", "9", ButtonStyle.Primary, null, null, false, row: 1);
+            builder.WithButton("0", "0", ButtonStyle.Primary, null, null, false, row: 1);
+
+            builder.WithButton("+", "+", ButtonStyle.Primary, null, null, false, row: 2);
+            builder.WithButton("-", "-", ButtonStyle.Primary, null, null, false, row: 2);
+            builder.WithButton("/", "/", ButtonStyle.Primary, null, null, false, row: 2);
+            builder.WithButton("*", "*", ButtonStyle.Primary, null, null, false, row: 2);
+            builder.WithButton("=", "=", ButtonStyle.Primary, null, null, false, row: 2);
+
+
+            await Context.Channel.SendMessageAsync("Calculator", component: builder.Build());
+            Context.Client.InteractionCreated += Client_InteractionCreated;
+
+        }
+
+
+        private async Task Client_InteractionCreated(SocketInteraction interaction)
+        {
+            switch (interaction)
             {
-            var builder = new ComponentBuilder().WithButton("Hello!", customId: "id_1", ButtonStyle.Secondary, row: 0);
-            await Context.Channel.SendMessageAsync("Test buttons!", component: builder.Build());
+                case SocketMessageComponent componentInteraction:
+                    await MyMessageComponentHandler(componentInteraction);
+                    break;
+
+                // Unused or Unknown/Unsupported
+                default:
+                    break;
             }
+        }
+        private async Task MyMessageComponentHandler(SocketMessageComponent interaction)
+        {
+
+            var customId = interaction.Data.CustomId;
+
+            var user = (SocketGuildUser)interaction.User;
+
+            var guild = user.Guild;
+            MessageProperties msgProps = new MessageProperties();
+            
+            await interaction.UpdateAsync(msgProps => msgProps.Content = $"{customId}");
+            
+            //await interaction.FollowupAsync($"Clicked {interaction.Data.CustomId}!", ephemeral: true);
+
         }
     }
 }
