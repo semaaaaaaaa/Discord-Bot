bc7340ea65132dd6c2bce82a5df6bd4231d72d6b
AdvellerdC <84400051+AdvellerdC@users.noreply.github.com>
2021-11-17 16:58:11 +0400

    Update

--- info.cs
+++ info.cs
@@ -16,31 +16,43 @@ namespace info
         [Command("info")]
         public async Task Info(SocketGuildUser user = null)
         {
-            if (user != null) {
-            string dataofcreation = user.CreatedAt.ToString();
-            bool isbot = user.IsBot;
-            string av = user.GetAvatarUrl();
-            var other = user.PublicFlags;
-            var other2 = user.Id;
-            var other3 = user.Status;
-            var embed = new EmbedBuilder();
-            embed.WithAuthor(user)
-            .WithFooter(footer => footer.Text = "User Informations")
-            .WithTitle($"Informations about {user}")
-            .WithThumbnailUrl(av)
-            .WithDescription($"\n\n **ID**: {other2} \n\n **Joined Discord**: {dataofcreation} \n\n **Joined Server**: {user.JoinedAt} \n\n**Is Bot**: {isbot.ToString()} \n\n **Badges**: {other} \n\n **Status**: {other3} \n\n **Is muted**: {user.IsMuted.ToString()}")
-            .WithCurrentTimestamp();
-            await ReplyAsync(embed: embed.Build());
+            if (user != null)
+            {
+                string dataofcreation = user.CreatedAt.Date.ToShortDateString();
+                bool isbot = user.IsBot;
+                string av = user.GetAvatarUrl();
+                var other = user.PublicFlags;
+                var other2 = user.Id;
+                var other3 = user.Status;
+                var embed = new EmbedBuilder();
+                embed.WithAuthor(user)
+                .WithFooter(footer => footer.Text = "User Informations")
+                .WithTitle($"Informations about {user}")
+                .WithThumbnailUrl(av)
+                .WithDescription($"\n **ID**: {other2} \n **Joined Discord**: {dataofcreation} \n **Joined Server**: {user.JoinedAt.Value.Date.ToShortDateString()} \n**Is Bot**: {isbot.ToString()} \n **Badges**: {other} \n"+" **Status**: "+ (other3.ToString() == "DoNotDisturb" ? "Do Not Disturb" : other3.ToString() == "Online" ? "Online" : other3.ToString() == "Idle" ? "Idle" : "Offline") +" \n")
+                .WithCurrentTimestamp();
+                await ReplyAsync(embed: embed.Build());
             }
-            else if (user == null) {
-            var embed2 = new EmbedBuilder();
-            embed2.WithAuthor(Context.User)
-            .WithFooter(footer => footer.Text = "Server Informations")
-            .WithTitle($"**{Context.Guild}**")
-            .WithThumbnailUrl(Context.Guild.IconUrl)
-            .WithDescription($"\n\n **ID**: {Context.Guild.Id} \n **Created at**: {Context.Guild.CreatedAt} \n **Rules channel**: {Context.Guild.RulesChannel} \n **Members**: {Context.Guild.MemberCount} \n")
-            .WithCurrentTimestamp();
-            await ReplyAsync(embed: embed2.Build());
+            else if (user == null)
+            {
+                 var role = Context.Guild.Roles;
+                 var value = String.Join(", ",role.AsEnumerable());
+                var embed2 = new EmbedBuilder();
+                embed2.WithAuthor(Context.User)
+                .WithFooter(footer => footer.Text = "Server Informations")
+                .WithTitle($"**{Context.Guild}**")
+                .WithThumbnailUrl(Context.Guild.IconUrl)
+                .WithDescription($"\n\n **ID**: {Context.Guild.Id} \n"
+                + $"**Created at**: {Context.Guild.CreatedAt} \n "
+                + $"**Rules channel**: {Context.Guild.RulesChannel.Mention}\n"
+                + $" **Members**: {Context.Guild.MemberCount} \n"
+                + $"**Owner**: {Context.Guild.Owner.Mention}\n"
+                +$"**Verification level**: {Context.Guild.VerificationLevel}\n"
+                +$"**2FA for Moderation**: {Context.Guild.MfaLevel}");
+                
+                embed2.WithCurrentTimestamp();
+                await ReplyAsync(embed: embed2.Build());
+                
             }
         }
     }
